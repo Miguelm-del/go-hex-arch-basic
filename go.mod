7c2ea60cdd6c185f495f844d463912026932d775
Miguel Márcio <mmms2@aluno.ifal.edu.br>
2023-01-26 15:32:01 -0300

    Add structure project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Miguelm-del/go-hex-arch-basic
+
+go 1.19
---
+++ src/internal/adapters/app/api/api.go
@@ -0,0 +1,43 @@
+package api
+
+import "github.com/Miguelm-del/go-hex-arch-basic/src/internal/ports"
+
+type Adapter struct {
+	arith ports.ArithmeticPort
+}
+
+func (apia Adapter) GetAddition(a, b int32) (int32, error) {
+	answer, err := apia.arith.Addition(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apia Adapter) GetSubtraction(a, b int32) (int32, error) {
+	answer, err := apia.arith.Subtraction(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apia Adapter) GetMultiplication(a, b int32) (int32, error) {
+	answer, err := apia.arith.Multiplication(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apia Adapter) GetDivision(a, b int32) (int32, error) {
+	answer, err := apia.arith.Division(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func NewAdapter(arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{arith: arith}
+}
